b6c42b776801c378ad781634cba4b2ab0d408fca
dlgPqls <63187777+dlgPqls@users.noreply.github.com>
2020-04-16 22:45:56 +0900

    Add files via upload

---
+++ 2주차과제_퀴즈.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	int k;
+
+	for (i = 0; i <=5; i++)
+	{
+		for (k = 0; k < i; k++)
+		{
+			cout << '*';
+			
+		}
+		cout << endl;
+	}
+}
