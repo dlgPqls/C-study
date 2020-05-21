e2b0cab40a625a96197328738a5d9c8ef14f4d5d
dlgPqls <63187777+dlgPqls@users.noreply.github.com>
2020-05-21 23:40:13 +0900

    Add files via upload

--- 스윙3.2.cpp
+++ 스윙3.2.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *num1, int *num2);
+void sort(int &arr[5});
+int print(int& arr[5]);
 
 int main()
 {
-	int x=0 ,y=0;
-	cout << "First Number : ";
-	cin >> x ;
-	cout << "Second Number : ";
-	cin >> y;
+	int arr[5] = 0;
+	int i;
 
-	cout << "swap 전 : " << x << "  " << y << endl;
+	cout << "숫자 다섯개를 입력해주세요. (띄어쓰기로 구분) : ";
+	for (i = 0; i < 5; i++)
+		cin >> arr[i];
 
-	swap(&x, &y);
+	cout << "<정렬 전>" << endl << endl;
+	print(arr[5]);
 
-	cout << "swap 후 : " << x << "  " << y << endl;
+	sort(arr[5]);
+
+	cout << "<정렬 전>" << endl << endl;
+	print(arr[5]);
 
 }
 
-void swap(int *num1, int *num2)
+void sort(int &arr[5})
 {
-	int tmp = 0;
+	int i, k, tmp = 0;
+	for (i = 0; i < 5; i++)
+		for (k = i + 1; k < 5; k++)
+			if (arr[i] > arr[k])
+			{
+				tmp = arr[i];
+				*arr[i] = arr[k];
+				*arr[k] = tmp;
 
-	tmp = *num1;
-	*num1 = *num2;
-	*num2 = tmp;
+			}
+				
+				
 }
+
+int print(int& arr[5])
+{
+	int i;
+
+	for (i = 1; i < 6; i++)
+	{
+		cout << i << arr[i] << endl;
+	}
+}
+
