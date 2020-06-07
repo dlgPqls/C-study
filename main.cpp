acd724fafbb622e7a37e77cb305a3e153f0a9711
dlgPqls <63187777+dlgPqls@users.noreply.github.com>
2020-06-07 17:43:21 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	phoneBook A;
+	int answer = 0;
+	cout << "--- swing 전화번호부 ---" << endl;
+	A.addPhone();
+
+	while (answer != 3) {
+		cout << "1. 최신 등록 전화번호 조회" << endl;
+		cout << "2. 최신 등록 전화번호 수정" << endl;
+		cout << "3. 종료 " << endl << " >> ";
+		cin >> answer;
+
+		switch (answer)
+		{
+		case 1:
+			cout << A.getName() << "::" << A.getPnum() << endl;
+			break;
+		case 2:
+			A.editPhone();
+			break;
+		}
+	}
+}
